9dba5f097c6a44070691e80571220435118f8ed9
munoztd0 <david.munoz@etu.unige.ch>
2020-12-04 18:42:48 +0100

    first

---
+++ R/utils.R
@@ -0,0 +1,4 @@
+# -------------------------------------- Miscellaneous  ----------------------------------------------------------
+
+
+scale2 <- function(x, na.rm = TRUE) (x - mean(x, na.rm = na.rm)) / sd(x, na.rm) # global functions
